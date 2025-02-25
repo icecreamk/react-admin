a2c62e9ea74f1af381520eba929c4258fe2de2e3
fflinkexin <fflinkexin@chinatelecom.cn>
2025-02-25 15:27:32 +0800

    1

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 - editorconfig : https://editorconfig.org/ （编辑器配置）
 - prettier : https://prettier.io/（代码格式化）
 - .vscode/settings.json (配置工程化)
+
+### router
+- react-router-dom
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+
+function Test() {
+  return <div>test</div>
+}
+
+function NotFound() {
+  return <div>not found</div>
+}
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/test" element={<Test />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </HashRouter>
 )
